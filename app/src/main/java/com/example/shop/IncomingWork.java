5590982cd2332a84fe72d9019a49c90c03f88460
rustam <rustamsobirov>
2021-04-11 09:55:48 +0500

    firt

--- app/src/main/java/com/example/shop/IncomingWork.java
+++ app/src/main/java/com/example/shop/IncomingWork.java
@@ -1,16 +1,45 @@
 package com.example.shop;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
 
 public class IncomingWork extends AppCompatActivity {
+
+    private Intent intent;
+    private ImageView barcodescan;
+    private ImageView add;
+
+    private ListView listView;
+    private SearchView searchView;
+    private ArrayList<STovar> list;
+    private STovarAdapter adapter;
+    private ProgressDialog progressDialog;
+    private String ip;
+    private User thisuUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incoming_work);
-
-//test
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Махсулотлар");
+        setSupportActionBar(toolbar);
+        add=findViewById(R.id.products_list_input_add);
+        barcodescan=findViewById(R.id.products_list_barcodescan);
+        listView=findViewById(R.id.products_list_list_view);
+        searchView=findViewById(R.id.searchView);
+        intent=getIntent();
+        ip=intent.getStringExtra("ip");
+        thisuUser=(User) intent.getSerializableExtra("user");
+        list=new ArrayList<>();
     }
 
 }
